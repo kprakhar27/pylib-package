ff8cd075c80ee925a05e7c9fd90d45bf4f615733
kprakhar27 <kumarprkhr@gmail.com>
2022-12-27 16:08:41 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+from kprakhar27_pylib import Multiplication
+
+# Instantiate a Multiplication object
+multiplication = Multiplication(2)
+
+# Call the multiply method
+print(multiplication.multiply(5))
