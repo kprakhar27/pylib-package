6cbc4aab93ca75ce16754cd91b4d572f82f2dd06
kprakhar27 <kumarprkhr@gmail.com>
2022-12-27 16:21:18 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -16,12 +16,12 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to setup() does all the work
 setup(
     name="kprakhar27_pylib",
-    version="0.1.1",
+    version="0.1.2",
     description="Demo library",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://kprakhar27_pylib.readthedocs.io/",
-    author="Joffrey Bienvenu",
+    author="Kumar Prakhar",
     author_email="kumarprkhr@gmail.com",
     license="MIT",
     classifiers=[
